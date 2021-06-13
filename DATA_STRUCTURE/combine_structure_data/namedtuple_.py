5bf60d6c004433e9f9b1af6c215634ed4e8781aa
turamant <tur1amant@gmail.com>
2021-06-13 23:21:05 +0300

    free code

--- DATA_STRUCTURE/combine_structure_data/namedtuple_.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-
-
-user = namedtuple("User", "name age gender")
-user1 = user(name='Serega', age=45, gender='male')
-print(user1.name, user1.age, user1.gender)
-print(user1[0], user1[1], user1[2])
